29273a38086cf3f27924292f7e67108e082d9596
vijayshigade0905 <shingadevijay89@gmail.com>
2024-09-22 11:50:45 +0530

    2nd commit

--- iconLook/iconLook/settings.py
+++ iconLook/iconLook/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'iconLook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iconlookdb',
+        'USER': 'root',
+        'PASSWORD': 'victory99',
+        'HOST':'localhost',
+        'PORT':'3306',
+
+
     }
 }
 
